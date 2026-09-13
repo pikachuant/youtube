2b6d00537ea431d717f8e44d205411224e6a9db8
Satyajit <satyajitswain78@gmail.com>
2026-09-13 19:01:35 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
 
 function Home() {
+    const [video,setVideo]=useState([])
+    const [error,setError]=useState("")
+    useEffect(()=>{
+       const checkVideo=async function() {
+        try {
+            const response=await fetch(
+                "https://antonpklive.online/v1/api/user/get-feed-videos",
+                {
+                    method:"GET",
+                    credentials:"include"
+                }
+            )
+            const data=await response.json()
+            if(data.success){
+                setVideo(data.data)
+            }else{
+                setError("failed Fetech Video Details")
+                return
+            }
+        } catch (error) {
+            setError(error)
+        }
+       }
+       
+     checkVideo()
+    },[])
   
   return (
-    <div>Home</div>
+    <>
+    <div>
+        {video.map((item)=>(
+            <Link 
+            to={`/watch/${item._id}`}
+            key={item._id}
+            >
+                <img
+                src={item.thumbnail}
+                alt={item.titile}
+                />
+                <span>{item.duration}</span>
+
+                <div>
+                    <h3>{item.titile}</h3>
+                    <p>{item.views ||0}views</p>
+                </div>
+            </Link>
+        ))}
+    </div>
+    </>
   )
 }
 
