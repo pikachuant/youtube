c45ba88bf13d4a34176c9c4607e23275168dcc24
Satyajit <satyajitswain78@gmail.com>
2026-09-13 19:13:18 +0530

    done

--- src/pages/Tweet.jsx
+++ src/pages/Tweet.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom'
 
 function Home() {
-    const [video,setVideo]=useState([])
+    const [tweet,setTweet]=useState([])
     const [error,setError]=useState("")
     useEffect(()=>{
        const checkVideo=async function() {
         try {
             const response=await fetch(
-                "https://antonpklive.online/v1/api/user/get-feed-videos",
+                "https://antonpklive.online/v1/api/user/user/tweet/all-tweet",
                 {
                     method:"GET",
                     credentials:"include"
@@ -16,22 +15,21 @@ function Home() {
             )
             const data=await response.json()
             if(data.success){
-                setVideo(data.data)
+                setTweet(data.data.Editable)
             }else{
-                setError("failed Fetech Video Details")
+                setError("failed Fetech Tweet Details")
                 return
             }
         } catch (error) {
             setError(error)
         }
        }
-
+       
      checkVideo()
     },[])
   
   return (
     <>
-    {error && <p>{error}</p>}
     <div>
         {video.map((item)=>(
             <Link 
