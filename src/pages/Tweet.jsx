e729a8d5f7765d1c54060a6e4c768985041e3d53
Satyajit <satyajitswain78@gmail.com>
2026-09-16 19:19:01 +0530

    hello

--- src/pages/Tweet.jsx
+++ src/pages/Tweet.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 function Home() {
     const [tweet,setTweet]=useState(null)
@@ -8,7 +8,42 @@ function Home() {
     const [editingTweetId,setEditingTweetId]=useState("")
     const [editingTweetContent,setEditingTweetContent]=useState("")
      
-    let url="https://antonpklive.online/v1/api/user/user/tweet/all-tweet"
+    const fetchTweet=useCallback(async function(){
+        if(!hasMore){
+            return
+        }
+        else{
+            try {
+                let url="https://antonpklive.online/v1/api/user/user/tweet/all-tweet"
+                if (cursor){
+                    url+=`?cursor=${cursor}`
+                }
+    
+                const response=await fetch(
+                    url,
+                    {
+                        method:"GET",
+                        credentials:"include"
+                    }
+                )
+                const data=await response.json()
+                if(!data.success){
+                    setError(data.message)
+                    return
+                }
+    
+                setTweet(...prev,
+                    ...data.data.Editable
+                )
+    
+                setCursor(data.data.cursor);
+                setHasMore(data.data.hasMore);
+            } catch (error) {
+                setError(error.message)
+            }
+
+        }
+    },[hasMore,cursor])
 
     useEffect(()=>{
        const checkTweet=async function() {
@@ -84,7 +119,7 @@ function Home() {
     <>
      {error && <p>{error}</p>}
      <div>
-        {tweet.map((tweets)=>(
+         {tweet.map((tweets)=>(
             <div id={tweets._id}>
                <h3>{tweets.content}</h3>
                 {
@@ -113,9 +148,21 @@ function Home() {
                         </div>
                     )
                 }
+
+                {
+                    hasMore &&(
+                        <div ref={loadMoreRef}></div>
+                    )
+                }
+
+                {
+                    !hasMore &&(
+                        <p>No More Tweets</p>
+                    )
+                }
             </div>
-        ))}
-     </div>
+         ))}
+      </div>
     </>
   )
 }
