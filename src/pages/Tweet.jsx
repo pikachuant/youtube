3653d18b21c7603ff7f77d8f85b2535121495ad3
Satyajit <satyajitswain78@gmail.com>
2026-09-16 18:40:22 +0530

    hello

--- src/pages/Tweet.jsx
+++ src/pages/Tweet.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react'
 
 function Home() {
-    const [tweet,setTweet]=useState([])
+    const [tweet,setTweet]=useState(null)
+    const [cursor, setCursor] = useState(null);
+    const [hasMore, setHasMore] = useState(null);
     const [error,setError]=useState("")
+    const [editingTweetId,setEditingTweetId]=useState("")
+    const [editingTweetContent,setEditingTweetContent]=useState("")
+     
+    let url="https://antonpklive.online/v1/api/user/user/tweet/all-tweet"
+
     useEffect(()=>{
        const checkTweet=async function() {
         try {
@@ -16,6 +23,8 @@ function Home() {
             const data=await response.json()
             if(data.success){
                 setTweet(data.data.Editable)
+                setCursor(data.data.cursor)
+                setHasMore(data.data.hasMore)
             }else{
                 setError("failed Fetech Tweet Details")
                 return
@@ -27,13 +36,86 @@ function Home() {
        
      checkTweet()
     },[])
+    
+    const EditTweet=async function(){
+        try {
+            const response=await fetch(
+                `https://antonpklive.online/v1/api/user/${editingTweetId}/edit-tweet`,
+                {
+                    method:'POST',
+                    headers:{
+                        "Content-Type": "application/json"
+                    },
+                    credentials:"include",
+                    body:JSON.stringify({
+                        content:editingTweetContent
+                    })
+                }
+            )
+            const data=await response.json()
+    
+            if(data.success){
+                setTweet(prev=>
+                    prev.map(t=>{
+                        t._id==editingTweetId?
+                        {
+                            ...t,
+                            content:data.data.content
+                            //inside a object literal 
+                        }
+                        :t
+                    })
+                )
+                setEditingTweetId(null);
+                setEditContent("");
+            }else{
+                setError(data.message)
+            }
+    
+    
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+   
   
+
   return (
     <>
-    {error && <p>{error}</p>}
-    <div>
-        {tweet.map()}
-    </div>
+     {error && <p>{error}</p>}
+     <div>
+        {tweet.map((tweets)=>(
+            <div id={tweets._id}>
+               <h3>{tweets.content}</h3>
+                {
+                  tweets.isEditable &&(
+                       <button onClick={()=>{
+                          setEditingTweetId(tweets._id)
+                          setEditingTweetContent(tweets.content)
+                         }
+                        }>
+                        Edit
+                       </button>
+                    )
+                }
+
+                {
+                    editingTweetId==tweets._id &&(
+                        <div>
+                            <textarea
+                            value={editingTweetContent}
+                            onChange={(e)=setEditingTweetContent(e.target.value)}
+                            />
+                            <button
+                            onClick={EditTweet}
+                            >Save</button>
+
+                        </div>
+                    )
+                }
+            </div>
+        ))}
+     </div>
     </>
   )
 }
