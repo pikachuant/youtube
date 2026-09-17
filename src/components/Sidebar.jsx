14d1a5f715287bbba81e60d8dd0eb65e396533bc
Satyajit <satyajitswain78@gmail.com>
2026-09-17 21:12:17 +0530

    hello

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Sidebar() {
+function Sidebar({ sidebarOpen }) {
+  if(!sidebarOpen){
+    return null
+  }
+
   return (
     <div>
 
